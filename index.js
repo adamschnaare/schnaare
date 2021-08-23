2a88e5b022346a587a9616d1b9cee431e2f1e12a
Adam Schnaare <1749981+adamschnaare@users.noreply.github.com>
2021-08-23 08:06:18 -0500

    👌 IMPROVE: Adds welcome header

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 #!/usr/bin/env node
-const clearConsole = require('clear-any-console');
+const welcome = require('cli-welcome');
+const pkg = require('./package.json');
 
-clearConsole();
+welcome({
+  title: pkg.name,
+  version: pkg.version,
+  tagLine: 'by Adam Schnaare',
+  description: pkg.description,
+  bgColor: `#FADC00`,
+  color: `#000000`,
+  bold: true,
+  clear: true,
+});
+
 console.log(`
-Adam Schnaare - Software Engineer Yes
+Adam Schnaare - Software Engineer
 
-Well, there is not much to say.
+A software engineer, dad, and horribly dry joke enthusiast.
 
-adam.schnaare@spinuptech.com
+✉️ adam.schnaare@spinuptech.com
 `);
