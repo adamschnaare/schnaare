cb5cb7176def0a290687f77367c4657132d55ed1
Adam Schnaare <1749981+adamschnaare@users.noreply.github.com>
2021-08-25 08:01:57 -0500

    📦 NEW: Colors

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 #!/usr/bin/env node
-const welcome = require('cli-welcome');
-const pkg = require('./package.json');
+const welcome = require('cli-welcome')
+const pkg = require('./package.json')
+const chalk = require('chalk')
+const log = console.log
+const emailColor = chalk.white.bold.inverse
+const githubColor = chalk.hex('#6cc644').bold.inverse
+const linkedInColor = chalk.hex('#00a0dc').bold.inverse
+const dim = chalk.dim
+const italic = chalk.dim.italic
 
 welcome({
-  title: pkg.name,
-  version: pkg.version,
-  tagLine: 'by Adam Schnaare',
-  description: pkg.description,
-  bgColor: `#FADC00`,
-  color: `#000000`,
-  bold: true,
-  clear: true,
-});
+    title: 'Adam Schnaare',
+    version: pkg.version,
+    tagLine: 'by Adam Schnaare',
+    description: pkg.description,
+    bgColor: `#6cc644`,
+    color: `#000000`,
+    bold: true,
+    clear: true,
+})
 
-console.log(`
-Adam Schnaare - Software Engineer
+log(`${italic(`A software engineer, dad, and horribly dry joke enthusiast.`)}
 
-A software engineer, dad, and horribly dry joke enthusiast.
-
-✉️ adam.schnaare@spinuptech.com
-`);
+${emailColor(' Email ')}    ${dim(`adam.schnaare@spinuptech.com`)}
+${githubColor(' Github ')}   ${dim('https://github.com/adamschnaare')}
+${linkedInColor(' LinkedIn ')} ${dim('https://www.linkedin.com/in/adamschnaare/')}
+`)
