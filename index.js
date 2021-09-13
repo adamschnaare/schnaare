d95a579cd15d0c3dc809c63737e56630b951145c
Adam Schnaare <1749981+adamschnaare@users.noreply.github.com>
2021-09-13 14:48:44 -0500

    📦 NEW: Check node version

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 #!/usr/bin/env node
-const welcome = require('cli-welcome')
-const pkg = require('./package.json')
-const chalk = require('chalk')
+import welcome from 'cli-welcome'
+import fs from 'fs'
+const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'))
+import chalk from 'chalk'
+import alert from 'cli-alert'
+import checkNode from 'cli-check-node'
+
 const log = console.log
 const emailColor = chalk.white.bold.inverse
 const githubColor = chalk.hex('#6cc644').bold.inverse
 const linkedInColor = chalk.hex('#00a0dc').bold.inverse
 const dim = chalk.dim
 const italic = chalk.dim.italic
+
+// alerts
+const success = msg => alert({ type: 'success', msg })
+const info = msg => alert({ type: 'info', msg })
+const warning = msg => alert({ type: 'warning', msg })
+const error = msg => alert({ type: 'error', msg })
+
+checkNode(`12`)
 
 welcome({
     title: 'Adam Schnaare',
@@ -26,3 +38,8 @@ ${emailColor(' Email ')}    ${dim(`adam.schnaare@spinuptech.com`)}
 ${githubColor(' Github ')}   ${dim('https://github.com/adamschnaare')}
 ${linkedInColor(' LinkedIn ')} ${dim('https://www.linkedin.com/in/adamschnaare/')}
 `)
+
+success('Thanks for checking this out')
+info(`I'm drinking coffee ☕️ right now`)
+warning('You are about to do something...not wise, young one')
+error('Well, that was our last ship across the pond, now, wasnt it')
